7880ef9928c4e1d637ca7b14cbd84d0fdd9007c3
Bipul Dubey <xs329-bipdub@xs-host603-027.xs.neuralcompany.team>
2023-08-07 18:59:29 +0530

    init react

---
+++ react-tut/firstapp/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+function App() {
+    return <div class="App"></div>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
