f072ab08a1000a674d42ea92c508542f2952de34
Bipul Dubey <>
2023-08-11 10:36:49 +0530

    react finish

--- routesdemo/src/App.js
+++ routesdemo/src/App.js
@@ -2,35 +2,34 @@ import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link, useParams, useNavigate } from 'react-router-dom';
 
 export default function App() {
-    const [isLoggedIn,setLoggedInState] = useState(false)
+    const [isLoggedIn, setLoggedInState] = useState(false)
 
     return <div className="App">
         <BrowserRouter>
-            <Header isLogin={isLoggedIn} onLogoutUpdate={setLoggedInState}/>
+            <Header isLogin={isLoggedIn} onLogoutUpdate={setLoggedInState} />
             <Routes>
 
-                {/* only before login */}
-                <Route exact path='/signin' element={<SignIn onLoginUpdate={setLoggedInState}/>} />
-                <Route exact path='/signup' element={<SignUp onLoginUpdate={setLoggedInState}/>} />
-                
+                <Route exact path='/signin' element={<SignIn onLoginUpdate={setLoggedInState} />} />
+                <Route exact path='/signup' element={<SignUp onLoginUpdate={setLoggedInState} />} />
+
                 {/* only after login */}
-                {isLoggedIn &&(
+                {isLoggedIn && (
                     <>
-                    <Route exact path='/about' element={<About />} />
-                    <Route exact path='/services' element={<Services />} />
+                        <Route exact path='/about' element={<About />} />
+                        <Route exact path='/services' element={<Services />} />
                     </>
                 )}
 
                 {/* anytime */}
                 <Route exact path='/' element={<Home />} />
-                <Route path='/:pageName' element={<PageNotFound/>}/>
+                <Route path='/:pageName' element={<PageNotFound />} />
 
             </Routes>
         </BrowserRouter>
     </div >
 }
 
-function Header({isLogin,onLogoutUpdate}) {
+function Header({ isLogin, onLogoutUpdate }) {
     const navigate = useNavigate()
 
     return <header className="header">
@@ -41,12 +40,12 @@ function Header({isLogin,onLogoutUpdate}) {
                 <li><Link className="link" to="/signin">Sign In</Link></li>
                 <li><Link className="link" to="/signup">Sign Up</Link></li>
             </>
-            :<>
-                <li><Link className="link" to="/">Home</Link></li>
-                <li><Link className="link" to="/about">About</Link></li>
-                <li><Link className="link" to="/services">Services</Link></li>
-            <li><Link className="link" to="/" onClick={() => {onLogoutUpdate(false); navigate("/"); }}>Sign out</Link></li>
-            </>}
+                : <>
+                    <li><Link className="link" to="/">Home</Link></li>
+                    <li><Link className="link" to="/about">About</Link></li>
+                    <li><Link className="link" to="/services">Services</Link></li>
+                    <li><Link className="link" to="/" onClick={() => { onLogoutUpdate(false); navigate("/"); }}>Sign out</Link></li>
+                </>}
         </ul>
     </header>
 }
@@ -57,19 +56,19 @@ function Home() {
     </main>
 }
 
-function SignUp({onLoginUpdate}){
+function SignUp({ onLoginUpdate }) {
     const navigate = useNavigate();
     return <main>
         <h3>This is Sign Up Page</h3>
-        <button  onClick={() => {onLoginUpdate(true); navigate("/"); }}>SignUp</button>
+        <button onClick={() => { onLoginUpdate(true); navigate("/"); }}>SignUp</button>
     </main>
 }
 
-function SignIn({onLoginUpdate}) {
+function SignIn({ onLoginUpdate }) {
     const navigate = useNavigate()
     return <main>
         <h3>This is Sign In Page</h3>
-        <button type='button' onClick={() => {onLoginUpdate(true); navigate("/"); }}>Login</button>
+        <button type='button' onClick={() => { onLoginUpdate(true); navigate("/"); }}>Login</button>
     </main>
 }
 
@@ -86,7 +85,7 @@ function Services() {
 }
 
 
-function PageNotFound(){
+function PageNotFound() {
     const params = useParams()
     return <div>{params.pageName} : page Not Found</div>
 }
